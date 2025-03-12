268eac06f1eaddd6e47dd8ec6a59853363a813a0
Rafał Szkopik <245936@edu.p.lodz.pl>
2025-03-12 09:52:43 +0100

    Dodanie własnego wyświetlania Rafał Szkopik

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String []args){
-        System.out.println("CodeBlack\nTester\nalbertbrozyna12");
+        System.out.println("SzkopikRafal");
     }
 }
