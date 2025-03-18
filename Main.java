23405a336a6bd7fbdf39af8eab26b1298fe71cf3
Jakub <245762@edu.p.lodz.pl>
2025-03-18 21:54:02 +0100

    revert "Dodałem komentarz do dzielenia"
    This reverts commit b8ff13f5b520da581b66d3e3aa02a04d1f7db7f4.
    
    Reverting changes

--- Main.java
+++ Main.java
@@ -13,7 +13,6 @@ interface IArithmeticsDiff{
     double Difference(double A, double B);
 }
 
-// Interface dividing two doubles
 interface IArithmeticDiv{
     double division(double a,double b);
 }
