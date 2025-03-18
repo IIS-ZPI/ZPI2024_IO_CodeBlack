f5482bd140569731a8457d5d3baca990c28e168e
Jakub <245762@edu.p.lodz.pl>
2025-03-18 20:25:24 +0100

    Implementacja interfejsu IArithmeticsMult

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
-public class Main {
+interface IArithmeticsMult{
+    double Multiplication(double A, double B);
+}
+
+public class Main implements IArithmeticsMult{
     public static  void main(String[] args){
         System.out.println("CodeBlack\n Tester/Scrum Master\n albertbrozyna12\nDeveloper\nDanielSzymczak\nDevOps\n kuba122388\nDeveloper\n SzkopikRafal\n");
     }
+
+    public double Multiplication(double A, double B) {
+        return A * B;
+    }
 }
-
-
