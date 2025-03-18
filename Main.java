3136b0e35ffda6892e3b4ce8ef40688a92b51409
DanielSzymczak <245941@edu.p.lodz.pl>
2025-03-18 20:29:08 +0100

    Implementacja interfejsu IArithmeticsDiff

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
+interface IArithmeticsDiff{
+    double Difference(double A, double B);
+}
+
 public class Main {
     public static  void main(String[] args){
         System.out.println("CodeBlack\n Tester/Scrum Master\n albertbrozyna12\nDeveloper\nDanielSzymczak\nDevOps\n kuba122388\nDeveloper\n SzkopikRafal\n");
     }
-}
 
+    public double Difference(double A, double B){
+        return A-B;
+    }
+}
 
