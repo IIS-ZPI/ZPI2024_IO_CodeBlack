9f778ca17e8cec1eebdcfa9fa0eacf5899c247b3
SzkopikRafal <245936@edu.p.lodz.pl>
2025-03-18 20:23:58 +0100

    Implementacja interfejsu IArithmeticsAdd w klasie Main

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
-public class Main {
+interface IArithmeticsAdd {
+    double add(double a, double b);
+}
+
+public class Main implements IArithmeticsAdd {
     public static  void main(String[] args){
         System.out.println("CodeBlack\n Tester/Scrum Master\n albertbrozyna12\nDeveloper\nDanielSzymczak\nDevOps\n kuba122388\nDeveloper\n SzkopikRafal\n");
     }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
 }
-
-
