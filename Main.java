a62ae8e20313ea4b3e1f3211b72ad69933261240
SzkopikRafal <245936@edu.p.lodz.pl>
2025-03-18 20:10:28 +0100

    Dodanie własnego wyświetlania

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String []args){
-        System.out.println("SzkopikRafal");
+        System.out.println("CodeBlack\n Developer\n SzkopikRafal\n");
     }
 }
