7a2d176c2dd70a5e58dfea7ec7dec204ac229ec3
Albert Brozyna <245779@edu.p.lodz.pl>
2025-03-18 20:23:51 +0100

    Implementacja interfejsu IArithmeticDiv w klasie Main.

--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
-public class Main {
-    public static  void main(String[] args){
-        System.out.println("CodeBlack\n Tester/Scrum Master\n albertbrozyna12\nDeveloper\nDanielSzymczak\nDevOps\n kuba122388\nDeveloper\n SzkopikRafal\n");
-    }
+
+interface IArithmeticDiv{
+    double division(double a,double b);
 }
 
+public class Main implements IArithmeticDiv{
+    public static  void main(String[] args){
+        System.out.println("CodeBlack\n Tester/Scrum Master\n albertbrozyna12\nDeveloper\nDanielSzymczak\nDevOps\n kuba122388\nDeveloper\n SzkopikRafal\n");
+
+
+    }
+
+    public double division(double a,double b){
+        if(b == 0){
+            return 0;
+        }
+        return a/b;
+    }
+}
 
