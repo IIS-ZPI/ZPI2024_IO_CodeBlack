913f6f0bd53828a2104a42e17ab37df27785a87f
Albert Brozyna <245779@edu.p.lodz.pl>
2025-05-20 19:48:20 +0200

    Added test for show currencies.

---
+++ tests/show_currencies_test.py
@@ -0,0 +1,11 @@
+from src.CurrencyManager import CurrencyManager
+from unittest.mock import patch
+from io import StringIO
+
+def test_show_available_currencies_output():
+    cm = CurrencyManager()
+    expected_output = "Available currencies:\n- USD\n- EUR\n- CHF\n- GBP\n- JPY\n- NOK\n- SEK\n"
+
+    with patch('sys.stdout', new=StringIO()) as fake_out:
+        cm.show_available_currencies()
+        assert fake_out.getvalue() == expected_output
